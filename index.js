90f2a4daa692f8d3c10507d048b54cea7cfdc25e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-26 23:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,26 @@ function increaseRankBy(n){ //need return in JS
   for (let i=0; i < array.length; i++){
     string= array[i].innerHTML
     array[i].innerHTML= parseInt(string,10) + n
-  
   }
     return array
 }
 function deepestChild(n){
+
+}
+function deepestChild(){
+
+  var grandnode= document.getElementById('grand-node')
+  var alldescendents= grandnode.querySelectorAll('*')
+  //let current=
+  //let next=[]
+  for (let i=0;i<alldescendents.length;i++){
+    if (alldescendents[i].childElementCount===0){
+      return grandnode.querySelectorAll('*')[i]
+      //doesnt work because even [3].hasChildNodes= true!
+      //but cant do [4] because only goes up to 3!
+    }
+
+  }
+  //document.querySelector('#grand-node div div div div')
 
 }
