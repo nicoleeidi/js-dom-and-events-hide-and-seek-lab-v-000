51c9a8cd5e6163374edefc0380413f532860779f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-26 23:23:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 function nestedTarget(){
-document.querySelector('#nested div div div.target')
+return document.querySelector('#nested div div div.target')
 }
 function increaseRankBy(n)
