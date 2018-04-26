77780391f756674482e1c07ddc2377c8b7204a61
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-26 23:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ function nestedTarget(){
 return document.querySelector('#nested div div div.target')
 }
 function increaseRankBy(n)
+array= document.querySelectorAll('.ranked-list')
 for (let i=0, i < array.length, i++)
