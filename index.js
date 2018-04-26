b15e3d6fd665eb4d10ae6a10d4373f308f8bf785
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-26 23:27:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function getFirstSelector(selector){
 function nestedTarget(){
 return document.querySelector('#nested div div div.target')
 }
-function increaseRankBy(n)
-array= document.querySelectorAll('.ranked-list')
-for (let i=0, i < array.length, i++)
+function increaseRankBy(n){
+  array= document.querySelectorAll('.ranked-list')
+  for (let i=0, i < array.length, i++)
+
+}
