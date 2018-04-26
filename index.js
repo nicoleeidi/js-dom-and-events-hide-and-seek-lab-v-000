4b35db6e95a7a165d621e46fa25c076dd18a173c
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-26 23:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ return document.querySelector('#nested div div div.target')
 function increaseRankBy(n){ //need return in JS
   let array= document.querySelectorAll('.ranked-list li')
   for (let i=0; i < array.length; i++){
-    string= array[i].innerHTML
+    let string= array[i].innerHTML
     array[i].innerHTML= parseInt(string,10) + n
   }
     return array
